6c53965029d36b835c9266d9db71f5192a3b164d
Andreas Mautsch <amautsch>
2023-11-05 08:30:17 +0100

    terraform changes

---
+++ terraform/infra/main.tf
@@ -0,0 +1,28 @@
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+
+}
+
+variable "hostname" {
+  default = "kubernetes"
+}
+
+variable "infra_repository" {
+  default = "https://goafabric.github.io/example-konstruction-kit/helm/charts/example/infra"
+}
+
+resource "helm_release" "welcome-board" {
+  repository = var.infra_repository
+  name       = "welcome-board"
+  chart      = "welcome-board"
+  version    = "1.1.1"
+  namespace  = "default"
+  create_namespace = true
+
+  set {
+    name  = "ingress.hosts"
+    value = var.hostname
+  }
+}
