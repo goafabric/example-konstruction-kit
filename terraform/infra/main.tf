72e8d59bde21e346e42fa6aad7f0038e21035953
Andreas Mautsch <amautsch>
2023-11-05 09:48:37 +0100

    terraform changes

--- terraform/infra/main.tf
+++ terraform/infra/main.tf
@@ -10,17 +10,3 @@ variable "hostname" {}
 variable "infra_repository" {
   default = "https://goafabric.github.io/example-konstruction-kit/helm/charts/example/infra"
 }
-
-resource "helm_release" "welcome-board" {
-  repository = var.infra_repository
-  name       = "welcome-board"
-  chart      = "welcome-board"
-  version    = "1.1.1"
-  namespace  = "default"
-  create_namespace = true
-
-  set {
-    name  = "ingress.hosts"
-    value = var.hostname
-  }
-}
