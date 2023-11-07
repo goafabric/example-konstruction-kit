112fa85d4d035d32846a748e37426bfa07bdb2f0
Andreas Mautsch <amautsch>
2023-11-07 15:30:00 +0100

    changes

--- terraform/infra2/dashboard.tf
+++ terraform/infra2/dashboard.tf
@@ -7,25 +7,43 @@ resource "helm_release" "kubernetes_dashboard" {
   wait       = false
 
   values = [file("dashboard-values.yml")]
-
-  set {
-    name  = "ingress.hosts[0]"
-    value = var.hostname  # Replace with your actual value
-  }
-
-  set {
-    name  = "ingress.tls[0].hosts[0]"
-    value = var.hostname  # Replace with your actual value
-  }
 }
 
 resource "kubernetes_manifest" "dashboard-ingress" {
   manifest   = yamldecode(
-    <<-EOF
+  <<-EOF
+  kind: Ingress
+  apiVersion: networking.k8s.io/v1
+  metadata:
+    name: kubernetes-dashboard
+    namespace: monitoring
+    annotations:
+      cert-manager.io/cluster-issuer: my-cluster-issuer
+      nginx.ingress.kubernetes.io/auth-secret: authentication-secret
+      nginx.ingress.kubernetes.io/auth-type: basic
+      nginx.ingress.kubernetes.io/backend-protocol: HTTPS
+      nginx.ingress.kubernetes.io/rewrite-target: /$1
+      service.alpha.kubernetes.io/app-protocols: '{"https":"HTTPS"}'
+  spec:
+    ingressClassName: nginx
+    tls:
+      - hosts:
+          - ${var.hostname}
+        secretName: root-certificate
+    rules:
+      - host: ${var.hostname}
+        http:
+          paths:
+            - path: /dashboard/?(.*)
+              pathType: ImplementationSpecific
+              backend:
+                service:
+                  name: kubernetes-dashboard
+                  port:
+                    number: 443
     EOF
   )
 }
-
 
 resource "kubernetes_manifest" "dashboard-role" {
   manifest   = yamldecode(
