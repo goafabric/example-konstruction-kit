0527b03f4cd0676fc4340e9a6c1777c357c267e0
Andreas Mautsch <amautsch>
2023-11-15 15:15:27 +0100

    changes

---
+++ terraform/argocd/controller/argocd.tf
@@ -0,0 +1,8 @@
+resource "helm_release" "argocd" {
+  name       = "argo-cd"
+  repository = "https://argoproj.github.io/argo-helm"
+  chart      = "argo-cd"
+  namespace  = "argocd"
+  create_namespace = true
+  version    = "5.51.2"
+}
