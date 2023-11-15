f8ed6390bf05f5a592fd11a048e90c1cfeb5b73d
Andreas Mautsch <amautsch>
2023-11-15 08:59:46 +0100

    changes

--- terraform/argocd/example/example.tf
+++ terraform/argocd/example/example.tf
@@ -32,66 +32,70 @@ resource "kubernetes_manifest" "callee-service-application" {
   )
 }
 
-#resource "kubernetes_manifest" "person-service-application" {
-#  manifest   = yamldecode(<<-EOF
-#    apiVersion: argoproj.io/v1alpha1
-#    kind: Application
-#    metadata:
-#      name: person-service-application
-#      namespace: argocd
-#    spec:
-#      project: default
-#      source:
-#        repoURL: ${var.helm_repository}
-#        targetRevision: refactoring
-#        path: helm/templates/example/spring/person-service/application
-#        helm:
-#          parameters:
-#            - name: "ingress.hosts"
-#              value: "${var.hostname}"
-#            - name: "image.arch"
-#              value: "-native${var.server_arch}"
-#            - name: "replicaCount"
-#              value: "1"
-#      destination:
-#        server: https://kubernetes.default.svc
-#        namespace: example
-#      syncPolicy:
-#        automated: {}
-#        syncOptions:
-#          - CreateNamespace=true
-#    EOF
-#  )
-#}
-#
-#resource "kubernetes_manifest" "person-service-postgres" {
-#  manifest   = yamldecode(<<-EOF
-#    apiVersion: argoproj.io/v1alpha1
-#    kind: Application
-#    metadata:
-#      name: person-service-postgres
-#      namespace: argocd
-#    spec:
-#      project: default
-#      source:
-#        repoURL: ${var.helm_repository}
-#        targetRevision: refactoring
-#        path: helm/templates/example/spring/person-service/postgres
-#        helm:
-#          parameters:
-#            - name: "ingress.hosts"
-#              value: "${var.hostname}"
-#            - name: "image.arch"
-#              value: "-native${var.server_arch}"
-#            - name: "replicaCount"
-#              value: "1"
-#      destination:
-#        server: https://kubernetes.default.svc
-#        namespace: example
-#      syncPolicy:
-#        automated: {}
-#        syncOptions:
-#          - CreateNamespace=true
-#    EOF
-#  )
-#}
+resource "kubernetes_manifest" "person-service-application" {
+  manifest   = yamldecode(<<-EOF
+    apiVersion: argoproj.io/v1alpha1
+    kind: Application
+    metadata:
+      name: person-service-application
+      namespace: argocd
+      finalizers:
+        - resources-finalizer.argocd.argoproj.io
+    spec:
+      project: default
+      source:
+        repoURL: ${var.helm_repository}
+        targetRevision: refactoring
+        path: helm/templates/example/spring/person-service/application
+        helm:
+          parameters:
+            - name: "ingress.hosts"
+              value: "${var.hostname}"
+            - name: "image.arch"
+              value: "-native${var.server_arch}"
+            - name: "replicaCount"
+              value: "1"
+      destination:
+        server: https://kubernetes.default.svc
+        namespace: example
+      syncPolicy:
+        automated: {}
+        syncOptions:
+          - CreateNamespace=true
+    EOF
+  )
+}
+
+resource "kubernetes_manifest" "person-service-postgres" {
+  manifest   = yamldecode(<<-EOF
+    apiVersion: argoproj.io/v1alpha1
+    kind: Application
+    metadata:
+      name: person-service-postgres
+      namespace: argocd
+      finalizers:
+        - resources-finalizer.argocd.argoproj.io
+    spec:
+      project: default
+      source:
+        repoURL: ${var.helm_repository}
+        targetRevision: refactoring
+        path: helm/templates/example/spring/person-service/postgres
+        helm:
+          parameters:
+            - name: "ingress.hosts"
+              value: "${var.hostname}"
+            - name: "image.arch"
+              value: "-native${var.server_arch}"
+            - name: "replicaCount"
+              value: "1"
+      destination:
+        server: https://kubernetes.default.svc
+        namespace: example
+      syncPolicy:
+        automated: {}
+        syncOptions:
+          - CreateNamespace=true
+    EOF
+  )
+}
