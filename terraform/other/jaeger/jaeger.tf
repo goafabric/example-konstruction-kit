db98593dfa30c52fc98c79475c2df9a9f30316e0
Andreas Mautsch <amautsch>
2023-11-28 10:30:22 +0100

    changes

---
+++ terraform/other/jaeger/jaeger.tf
@@ -0,0 +1,68 @@
+resource "helm_release" "jaeger" {
+  repository = "https://jaegertracing.github.io/helm-charts"
+  name       = "jaeger"
+  chart      = "jaeger"
+  version    = "0.72.1"
+  namespace  = "monitoring"
+  create_namespace = true
+
+  set {
+    name  = "allInOne.enabled"
+    value = "true"
+  }
+  set {
+    name  = "provisionDataStore.cassandra"
+    value = "false"
+  }
+  set {
+    name  = "storage.type"
+    value = "none"
+  }
+  set {
+    name  = "agent.enabled"
+    value = "false"
+  }
+  set {
+    name  = "collector.enabled"
+    value = "false"
+  }
+  set {
+    name  = "query.enabled"
+    value = "false"
+  }
+
+}
+
+/*
+resource "kubernetes_manifest" "jaeger-ingress" {
+  manifest   = yamldecode(
+    <<-EOF
+  kind: Ingress
+  apiVersion: networking.k8s.io/v1
+  metadata:
+    name: jaeger
+    namespace: monitoring
+    annotations:
+      cert-manager.io/cluster-issuer: my-cluster-issuer
+      nginx.ingress.kubernetes.io/rewrite-target: /jaeger/$1
+  spec:
+    ingressClassName: nginx
+    tls:
+      - hosts:
+          - ${var.hostname}
+        secretName: root-certificate
+    rules:
+      - host: ${var.hostname}
+        http:
+          paths:
+            - path: /jaeger/?(.*)
+              pathType: ImplementationSpecific
+              backend:
+                service:
+                  name: tracing
+                  port:
+                    number: 80
+  EOF
+  )
+}
+*/
