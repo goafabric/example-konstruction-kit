beaee5b6da697bdbb9410a942ac23821b9f8e541
Andreas Mautsch <amautsch>
2023-11-29 14:00:15 +0100

    changes

---
+++ terraform/infra/loki/grafana.tf
@@ -0,0 +1,49 @@
+resource "helm_release" "grafana" {
+  repository = "https://grafana.github.io/helm-charts"
+  name       = "grafana"
+  chart      = "grafana"
+  version    = "6.57.0"
+  namespace  = "monitoring"
+  create_namespace = true
+
+  values = [file("values.yaml")]
+
+  set {
+    name  = "ingress.hosts[0]"
+    value = var.hostname
+  }
+  set {
+    name  = "ingress.tls[0].hosts[0]"
+    value = var.hostname
+  }
+}
+
+resource "helm_release" "loki" {
+  repository = "https://grafana.github.io/helm-charts"
+  name       = "loki"
+  chart      = "loki-stack"
+  version    = "v2.8.9"
+  namespace  = "monitoring"
+  create_namespace = true
+
+  set {
+    name  = "grafana.enabled"
+    value = "true"
+  }
+  # no prometheus is of course invalid for production
+  set {
+    name  = "prometheus.enabled"
+    value = "false"
+  }
+  set {
+    name  = "prometheus.alertmanager.persistentVolume.enabled"
+    value = "false"
+  }
+  set {
+    name  = "prometheus.server.persistentVolume.enabled"
+    value = "false"
+  }
+}
+
+
+
