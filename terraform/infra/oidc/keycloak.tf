4f930cdb7813ad9d7a1a333b88191a38a83e7be0
Andreas Mautsch <amautsch>
2023-11-30 09:21:02 +0100

    changes

--- terraform/infra/oidc/keycloak.tf
+++ terraform/infra/oidc/keycloak.tf
@@ -27,3 +27,11 @@ resource "helm_release" "keycloak-service-postgres" {
     value = var.hostname
   }
 }
+
+resource "terraform_data" "keycloak_users" {
+  depends_on = [helm_release.keycloak-service-application]
+  provisioner "local-exec" {
+    when    = create
+    command = "./scripts/create-users https://${var.hostname}"
+  }
+}
