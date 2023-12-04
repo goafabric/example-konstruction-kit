5b86c887237d4d8884459607d85e6b933a4e73d3
Andreas Mautsch <amautsch>
2023-12-04 15:24:25 +0100

    changes

--- terraform/server/kind/main.tf
+++ terraform/server/kind/main.tf
@@ -1,16 +1,11 @@
 resource "terraform_data" "kind" {
   provisioner "local-exec" {
     when = create
-    command = "./server-create"
+    command = "./kind-create"
   }
 
   provisioner "local-exec" {
     when = destroy
     command = "kind delete cluster"
   }
-
 }
-
-# curl https://kubernetes:32443/welcome/
-
-
