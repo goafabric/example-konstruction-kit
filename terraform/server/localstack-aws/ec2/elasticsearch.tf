aa9f7206b1c32f9c7f6088b87f7d3014176369c0
Andreas Mautsch <amautsch>
2023-12-07 16:19:22 +0100

    aws added

--- terraform/server/localstack-aws/ec2/elasticsearch.tf
+++ terraform/server/localstack-aws/ec2/elasticsearch.tf
@@ -2,9 +2,9 @@ resource "aws_elasticsearch_domain" "example" {
   domain_name           = "goafabric"
   elasticsearch_version = "7.10"
 
-#  cluster_config {
-#    instance_type = "r4.large.elasticsearch"
-#  }
+  cluster_config {
+    instance_type = "r4.large.elasticsearch"
+  }
 
   tags = {
     Domain = "TestDomain"
