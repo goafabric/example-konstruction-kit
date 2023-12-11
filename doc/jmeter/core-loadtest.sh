c1e3ebb7e7d0f2a4ef763bf6a59dc3c39dfbe0bd
Andreas Mautsch <amautsch>
2023-12-11 16:01:30 +0100

    upgrades

--- doc/jmeter/core-loadtest.sh
+++ doc/jmeter/core-loadtest.sh
@@ -29,18 +29,3 @@ do
 done
 
 #docker run --pull always --name catalog-batch-service --rm -p50600:50600 -e 'management.otlp.tracing.endpoint=http://host.docker.internal:4318/v1/traces' goafabric/catalog-batch-service:1.2.0-SNAPSHOT
-
-
-practitioner_info=$(curl -k -s "${baseurl}/practitioners/findByFamilyName?familyName=Hibbert")
-
-practitioner_info=$(curl -k -s "${baseurl}/practitioners/findByFamilyName?familyName=Hibbert") && curl -k -X POST -H "Content-Type: application/json" -d '{
-  "id": "'$(echo "$practitioner_info" | jq -r '.[0].id')'",
-  "version": "'$(echo "$practitioner_info" | jq -r '.[0].version')'",
-  "givenName": "Dr. Julius",
-  "familyName": "Hibbert",
-  "gender": "male",
-  "birthDate": "2020-01-08",
-  "lanr": "4711"
-}' "${baseurl}/practitioners/save"
-
-curl -k -s "${baseurl}/practitioners/findByFamilyName?familyName=Hibbert"
