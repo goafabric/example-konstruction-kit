323b7d4a4af6fb8a5b5063d99731ae8dfef4a4b3
Andreas Mautsch <amautsch>
2024-03-24 14:29:45 +0100

    changes

--- terraform/mesh/istio/istio.tf
+++ terraform/mesh/istio/istio.tf
@@ -2,7 +2,6 @@ resource "terraform_data" "istio" {
   provisioner "local-exec" {
     when = create
     command = "istioctl install -y --set profile=${var.profile} --set values.cni.cniConfDir=/var/snap/microk8s/current/args/cni-network --set values.cni.cniBinDir=/var/snap/microk8s/current/opt/cni/bin -f ./templates/meshconfig.yaml"
-
   }
 
   provisioner "local-exec" {
