3c669d60a9b3b86e85883093153514f3a7b53a39
Andreas Mautsch <amautsch>
2024-03-28 07:39:50 +0100

    changes

--- terraform/core/core.tf
+++ terraform/core/core.tf
@@ -32,18 +32,3 @@ resource "helm_release" "core-application" {
     value = random_password.s3_password.result
   }
 }
-
-resource "helm_release" "core-postgres" {
-  repository = var.helm_repository
-  name       = "core-postgres"
-  chart      = "${var.helm_repository}/core/postgres"
-  version    = "1.1.2"
-  namespace  = "core"
-  create_namespace = true
-  timeout = var.helm_timeout
-
-  set {
-    name  = "database.password"
-    value = random_password.database_password.result
-  }
-}
