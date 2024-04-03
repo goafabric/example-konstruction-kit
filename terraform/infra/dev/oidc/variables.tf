07dd8938455074b8fed26bcfda750ccac08163ec
Andreas Mautsch <amautsch>
2024-04-03 13:15:01 +0200

    keycloak changes

--- terraform/infra/dev/oidc/variables.tf
+++ terraform/infra/dev/oidc/variables.tf
@@ -9,3 +9,8 @@ variable "helm_timeout" {
 variable "infra_repository" {
   default = "../../../../helm/templates/infra" #"https://goafabric.github.io/example-konstruction-kit/helm/charts/example/infra"
 }
+
+locals {
+  production_mode = var.hostname == "kind" ? "false" : "true"
+  base_url = local.production_mode == "true" ? "https://${var.hostname}:443" : "https://${var.hostname}:32443"
+}
