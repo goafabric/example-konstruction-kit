1d5b73a586252ee19a987acedbfc27c1f49f373a
Andreas Mautsch <amautsch>
2024-04-23 09:13:08 +0200

    changes

--- terraform/infra/grafana/grafana.tf
+++ terraform/infra/grafana/grafana.tf
@@ -25,3 +25,38 @@ resource "helm_release" "grafana" {
     value = "admin"
   }
 }
+
+resource "kubernetes_manifest" "grafana-gateway" {
+  manifest   = yamldecode(<<-EOF
+  kind: ApisixRoute
+  apiVersion: apisix.apache.org/v2
+  metadata:
+    name: grafana
+    namespace: monitoring
+  spec:
+    http:
+      - name: grafana
+        match:
+          hosts:
+            - ${var.hostname}
+          paths:
+            - /grafana
+            - /grafana/*
+        websocket: true
+        backends:
+          - serviceName: grafana
+            servicePort: 80
+        plugins:
+          - name: redirect
+            enable: true
+            config:
+              http_to_https: true
+          - name: proxy-rewrite
+            enable: true
+            config:
+              regex_uri:
+                - /grafana/(.*)
+                - /$1
+  EOF
+  )
+}
