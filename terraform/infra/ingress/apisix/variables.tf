3f9553e46d9386b88c9bb97e50d5056a35b15e3b
Andreas Mautsch <amautsch>
2024-04-26 14:20:33 +0200

    changes

--- terraform/infra/ingress/apisix/variables.tf
+++ terraform/infra/ingress/apisix/variables.tf
@@ -5,6 +5,6 @@ variable "hostname" {
 locals {
   production_mode = !strcontains(var.hostname, ".local")
 
-  cert_manager_issuer = "./cert-manager-issuer/selfsigned" #"true" ? "./cert-manager-issuer/letsencrypt" : "./cert-manager-issuer/selfsigned"
+  cert_manager_issuer = "./cert-manager-issuer/selfsigned" #local.production_mode == true ? "./cert-manager-issuer/letsencrypt" : "./cert-manager-issuer/selfsigned"
   ingress_service_type = local.production_mode == true ? "LoadBalancer" : "NodePort"
 }
