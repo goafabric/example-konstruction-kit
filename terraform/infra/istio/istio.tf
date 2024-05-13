3f12772825f434aa0bf9a46638b0c1a50a82072b
Andreas Mautsch <amautsch>
2024-05-13 15:32:39 +0200

    istio upgrade

--- terraform/infra/istio/istio.tf
+++ terraform/infra/istio/istio.tf
@@ -1,12 +1,12 @@
 resource "terraform_data" "istio" {
   provisioner "local-exec" {
     when = create
-    command = "istioctl-beta install -y --set profile=ambient -f ./templates/meshconfig.yaml"
+    command = "istioctl install -y --set profile=ambient -f ./templates/meshconfig.yaml"
     #command = "istioctl install -y --set profile=${var.profile} --set values.cni.cniConfDir=/var/snap/microk8s/current/args/cni-network --set values.cni.cniBinDir=/var/snap/microk8s/current/opt/cni/bin -f ./templates/meshconfig.yaml"
   }
 
   provisioner "local-exec" {
     when = destroy
-    command = "istioctl-beta uninstall -y --purge"
+    command = "istioctl uninstall -y --purge"
   }
 }
