866b7d778ca09b4b2d244dfa2f6ec017cafe8709
Andreas Mautsch <amautsch>
2024-05-14 08:58:26 +0200

    istio upgrade

--- terraform/infra/istio/label.sh
+++ terraform/infra/istio/label.sh
@@ -2,5 +2,6 @@
 kubectl create namespace core; kubectl label namespace core "istio.io/dataplane-mode"="ambient"
 kubectl create namespace example; kubectl label namespace example "istio.io/dataplane-mode"="ambient"
 kubectl create namespace message-broker; kubectl label namespace message-broker "istio.io/dataplane-mode"="ambient"
+kubectl create namespace oidc; kubectl label namespace message-broker "istio.io/dataplane-mode"="ambient"
 
 #kubectl label namespace ingress-apisix "istio.io/dataplane-mode"="ambient"
