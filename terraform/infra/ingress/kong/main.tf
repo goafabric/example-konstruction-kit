ee747ad3575709e399fff75e18289db9a5a47867
Andreas Mautsch <amautsch>
2024-05-17 12:02:24 +0200

    cert-manager

--- terraform/infra/ingress/kong/main.tf
+++ terraform/infra/ingress/kong/main.tf
@@ -7,6 +7,3 @@ provider "helm" {
 provider "kubernetes" {
   config_path = "~/.kube/config"
 }
-
-# kubectl apply -f https://github.com/kubernetes-sigs/gateway-api/releases/download/v1.0.0/standard-install.yaml && kubectl apply -f ./templates/gateway-class.yaml
-# kubectl delete -f https://github.com/kubernetes-sigs/gateway-api/releases/download/v1.0.0/standard-install.yaml && kubectl delete -f ./templates/gateway-class.yaml
