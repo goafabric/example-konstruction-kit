8b2c40ab25d397bf60bea74876cb6a770f2ba9ec
Andreas Mautsch <amautsch>
2024-05-17 07:36:17 +0200

    kong added

---
+++ terraform/infra/ingress/kong/kong.tf
@@ -0,0 +1,20 @@
+resource "helm_release" "kong" {
+  name             = "kong"
+  repository       = "https://charts.konghq.com"
+  chart            = "kong"
+  version          = "2.38.0"
+  namespace        = "kong"
+  timeout          = "120"
+  create_namespace = true
+
+  set {
+   name  = "proxy.type"
+   value = local.ingress_service_type
+  }
+
+  set {
+    name  = "proxy.tls.nodePort"
+    value = "32443"
+  }
+}
+
