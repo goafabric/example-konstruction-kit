897bc847c125b2d59f16704b5eec84acee00c2dc
Andreas Mautsch <amautsch>
2024-05-17 13:43:32 +0200

    cert-manager

--- terraform/infra/ingress/kong/gateway-crd.tf
+++ terraform/infra/ingress/kong/gateway-crd.tf
@@ -1,23 +1,3 @@
-resource "helm_release" "kong" {
-  name             = "kong"
-  repository       = "https://charts.konghq.com"
-  chart            = "kong"
-  version          = "2.38.0"
-  namespace        = "kong"
-  timeout          = "120"
-  create_namespace = true
-
-  set {
-   name  = "proxy.type"
-   value = local.ingress_service_type
-  }
-
-  set {
-    name  = "proxy.tls.nodePort"
-    value = "32443"
-  }
-}
-
 resource "terraform_data" "gateway_crd" {
   provisioner "local-exec" {
     when = create
@@ -29,4 +9,3 @@ resource "terraform_data" "gateway_crd" {
     command = "kubectl delete -f ./templates/gateway-class.yaml && kubectl delete -f https://github.com/kubernetes-sigs/gateway-api/releases/download/v1.0.0/standard-install.yaml"
   }
 }
-
