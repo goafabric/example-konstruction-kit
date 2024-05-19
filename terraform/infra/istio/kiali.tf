09b53de2b2d21d02d666a17f5515c317300bf2c2
Andreas Mautsch <amautsch>
2024-05-19 08:13:58 +0200

    kong

--- terraform/infra/istio/kiali.tf
+++ terraform/infra/istio/kiali.tf
@@ -58,10 +58,57 @@ resource "kubernetes_manifest" "kiali-route" {
   )
 }
 
+resource "kubernetes_manifest" "kiali-gateway" {
+  manifest   = yamldecode(<<-EOF
+  apiVersion: gateway.networking.k8s.io/v1
+  kind: Gateway
+  metadata:
+    name: kiali-gateway
+    namespace: istio-system
+  spec:
+    gatewayClassName: kong
+    listeners:
+      - name: https
+        port: 443
+        protocol: HTTPS
+        hostname: kind.local
+        tls:
+          mode: Terminate
+          certificateRefs:
+            - kind: Secret
+              name: root-certificate
+  EOF
+  )
+}
+
+resource "kubernetes_manifest" "kiali-httproute" {
+  manifest   = yamldecode(<<-EOF
+  apiVersion: gateway.networking.k8s.io/v1
+  kind: HTTPRoute
+  metadata:
+    name: kiali-route
+    namespace: istio-system
+  spec:
+    parentRefs:
+      - name: kiali-gateway
+        sectionName: https
+    hostnames:
+      - kind.local
+    rules:
+      - matches:
+          - path:
+              type: PathPrefix
+              value: /kiali
+        backendRefs:
+          - name: kiali
+            port: 20001
+  EOF
+  )
+}
 
 
 resource "terraform_data" "prometheus" {
-  depends_on = [kubernetes_manifest.kiali-ingress]
+  depends_on = [helm_release.kiali]
   provisioner "local-exec" {
     when = create
     command = "kubectl apply -f ./templates/prometheus.yaml"
