b6fc8b8bf6186e9b2765345bf3942058f9519de8
Andreas Mautsch <amautsch>
2024-05-19 16:36:56 +0200

    kong

--- terraform/infra/istio/kiali.tf
+++ terraform/infra/istio/kiali.tf
@@ -135,3 +135,16 @@ resource "kubernetes_manifest" "kiali-ingress" {
   EOF
   )
 }
+
+resource "terraform_data" "prometheus" {
+  depends_on = [helm_release.kiali]
+  provisioner "local-exec" {
+    when = create
+    command = "kubectl apply -f ./templates/prometheus.yaml"
+  }
+
+  provisioner "local-exec" {
+    when = destroy
+    command = "kubectl delete --ignore-not-found -f ./templates/prometheus.yaml"
+  }
+}
