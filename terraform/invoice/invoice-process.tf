eee14c447b9ad81ecd3dc47836e68073afa27502
Andreas Mautsch <amautsch>
2024-05-22 13:39:41 +0200

    invoice added

---
+++ terraform/invoice/invoice-process.tf
@@ -0,0 +1,30 @@
+resource "helm_release" "invoice-process-application" {
+  repository = var.helm_repository
+  name       = "invoice-process-application"
+  chart      = "${var.helm_repository}/invoice-process/application"
+  namespace  = "invoice"
+  create_namespace = true
+  timeout = var.helm_timeout
+
+  set {
+    name  = "replicaCount"
+    value = local.replica_count
+  }
+
+  set {
+    name  = "ingress.hosts"
+    value = var.hostname
+  }
+  set {
+    name  = "image.arch"
+    value = "-native${local.server_arch}"
+  }
+  set {
+    name = "authentication.enabled"
+    value = local.authentication_enabled
+  }
+  set {
+    name = "service.password"
+    value = random_password.service_password.result
+  }
+}
