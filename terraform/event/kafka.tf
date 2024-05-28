f3f74b101b7e3b1b1de3c106832111d345baf4d3
Andreas Mautsch <amautsch>
2024-05-28 15:27:28 +0200

    changes

--- terraform/event/kafka.tf
+++ terraform/event/kafka.tf
@@ -14,3 +14,43 @@ resource "helm_release" "kafka" {
 #   }
 }
 
+
+resource "helm_release" "kafka-ha" {
+  count = local.message_broker_ha == "true" ? 1 : 0
+
+  name       = "kafka"
+  repository = "https://charts.bitnami.com/bitnami"
+  chart      = "kafka"
+  version    = "29.1.1"
+  namespace  = "event"
+  create_namespace = "true"
+
+  set {
+    name = "controller.replicaCount"
+    value = local.replica_count
+  }
+  set {
+    name = "controller.heapOpts"
+    value = "-Xmx256m"
+  }
+
+  set {
+    name = "listeners.client.protocol"
+    value = "PLAINTEXT"
+  }
+
+  set {
+    name  = "persistence.size"
+    value = "2Gi"
+  }
+}
+
+# manually remove the pvc to avoid password problems
+resource "terraform_data" "remove_postgres_pvc" {
+  count = local.message_broker_ha == "true" ? 1 : 0
+
+  provisioner "local-exec" {
+    when = destroy
+    command = "kubectl delete pvc -l app.kubernetes.io/instance=kafka -n event"
+  }
+}
--- terraform/event/variables.tf
+++ terraform/event/variables.tf
@@ -20,5 +20,5 @@ locals {
   oidc_enabled = local.production_mode
 
   replica_count = local.production_mode ? "2" : "1"
-  message_broker_ha = "false"
+  message_broker_ha = "true"
 }
