7ea05f629cde19b1c39de281fe2b976ca44ad9f1
Andreas Mautsch <amautsch>
2024-05-30 11:57:44 +0200

    changes

--- terraform/event/event-dispatcher.tf
+++ terraform/event/event-dispatcher.tf
@@ -1,4 +1,7 @@
-resource "helm_release" "event-dispatcher-service-application" {
+resource "helm_release" "event-dispatcher-service-application-kafka" {
+  count = local.dispatcher_profile == "kafka" ? 1 : 0
+  depends_on = [helm_release.kafka]
+
   repository = var.helm_repository
   name       = "event-dispatcher-service-application"
   chart      = "${var.helm_repository}/event-dispatcher-service/application"
@@ -6,13 +9,10 @@ resource "helm_release" "event-dispatcher-service-application" {
   create_namespace = true
   timeout = var.helm_timeout
 
-#  depends_on = [helm_release.kafka]
-
   set {
     name  = "replicaCount"
     value = local.replica_count
   }
-
   set {
     name  = "ingress.hosts"
     value = var.hostname
@@ -33,5 +33,41 @@ resource "helm_release" "event-dispatcher-service-application" {
     name  = "image.arch"
     value = local.dispatcher_profile == "kafka" ? "-native${local.server_arch}" : ""
   }
+}
 
+resource "helm_release" "event-dispatcher-service-application-rabbit" {
+  count = local.dispatcher_profile == "rabbitmq" ? 1 : 0
+  depends_on = [helm_release.rabbitmq]
+
+  repository = var.helm_repository
+  name       = "event-dispatcher-service-application"
+  chart      = "${var.helm_repository}/event-dispatcher-service/application"
+  namespace  = "event"
+  create_namespace = true
+  timeout = var.helm_timeout
+
+  set {
+    name  = "replicaCount"
+    value = local.replica_count
+  }
+  set {
+    name  = "ingress.hosts"
+    value = var.hostname
+  }
+  set {
+    name  = "messageBroker.password"
+    value = random_password.messageBroker_password.result
+  }
+  set {
+    name = "oidc.enabled"
+    value = local.oidc_enabled
+  }
+  set {
+    name = "dispatcher.profile"
+    value = local.dispatcher_profile
+  }
+  set {
+    name  = "image.arch"
+    value = local.dispatcher_profile == "kafka" ? "-native${local.server_arch}" : ""
+  }
 }
--- terraform/event/rabbitmq.tf
+++ terraform/event/rabbitmq.tf
@@ -1,56 +1,39 @@
 resource "helm_release" "rabbitmq" {
   count = local.dispatcher_profile == "rabbitmq" ? 1 : 0
-  
-  repository = var.helm_repository
+
   name       = "rabbitmq"
-  chart      = "${var.helm_repository}/rabbitmq/application"
+  repository = "https://charts.bitnami.com/bitnami"
+  chart      = "rabbitmq"
+  version    = "13.0.0"
   namespace  = "event"
-  create_namespace = true
-  timeout = var.helm_timeout
+  create_namespace = "true"
 
   set {
-    name  = "messageBroker.password"
+    name = "replicaCount"
+    value = local.messageBroker_replica_count
+  }
+  set {
+    name  = "persistence.size"
+    value = "2Gi"
+  }
+
+  set {
+    name  = "auth.username"
+    value = "admin"
+  }
+
+  set {
+    name  = "auth.password"
     value = random_password.messageBroker_password.result
   }
 }
 
+# manually remove the pvc to avoid password problems
+resource "terraform_data" "remove_rabbitmq_pvc" {
+  count = local.dispatcher_profile == "rabbitmq" ? 1 : 0
 
-# resource "helm_release" "rabbitmq" {
-#   count = local.dispatcher_profile == "rabbitmq" ? 1 : 0
-#
-#   name       = "rabbitmq"
-#   repository = "https://charts.bitnami.com/bitnami"
-#   chart      = "rabbitmq"
-#   version    = "13.0.0"
-#   namespace  = "event"
-#   create_namespace = "true"
-#
-#   set {
-#     name = "replicaCount"
-#     value = local.messageBroker_replica_count
-#   }
-#   set {
-#     name  = "persistence.size"
-#     value = "2Gi"
-#   }
-#
-#   set {
-#     name  = "auth.username"
-#     value = "admin"
-#   }
-#
-#   set {
-#     name  = "auth.password"
-#     value = random_password.messageBroker_password.result
-#   }
-# }
-#
-# # manually remove the pvc to avoid password problems
-# resource "terraform_data" "remove_rabbitmq_pvc" {
-#   count = local.dispatcher_profile == "rabbitmq" ? 1 : 0
-#
-#   provisioner "local-exec" {
-#     when = destroy
-#     command = "kubectl delete pvc -l app.kubernetes.io/instance=rabbitmq -n event"
-#   }
-# }
+  provisioner "local-exec" {
+    when = destroy
+    command = "kubectl delete pvc -l app.kubernetes.io/instance=rabbitmq -n event"
+  }
+}
