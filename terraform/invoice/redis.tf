d285c86c4c012f6240c3110ab52cf2700c26351e
Andreas Mautsch <amautsch>
2024-06-10 15:50:37 +0200

    changes

--- terraform/invoice/redis.tf
+++ terraform/invoice/redis.tf
@@ -1,8 +1,42 @@
+# resource "helm_release" "redis" {
+#   repository = var.helm_repository
+#   name       = "redis"
+#   chart      = "${var.helm_repository}/invoice-process/redis"
+#   namespace  = "invoice"
+#   create_namespace = true
+#   timeout = var.helm_timeout
+# }
+
+
 resource "helm_release" "redis" {
-  repository = var.helm_repository
   name       = "redis"
-  chart      = "${var.helm_repository}/invoice-process/redis"
+  repository = "https://charts.bitnami.com/bitnami"
+  chart      = "redis"
   namespace  = "invoice"
-  create_namespace = true
-  timeout = var.helm_timeout
+  version    = "19.5.2"
+
+  set {
+    name  = "master.persistence.size"
+    value = "2Gi"
+  }
+  set {
+    name  = "replica.persistence.size"
+    value = "2Gi"
+  }
+  set {
+    name  = "architecture"
+    value = "standalone"
+  }
+  set {
+    name  = "auth.enabled"
+    value = false
+  }
+
+}
+
+resource "terraform_data" "remove_redis_pvc" {
+  provisioner "local-exec" {
+    when    = destroy
+    command = "kubectl delete pvc -l app.kubernetes.io/instance=redis -n invoice"
+  }
 }
