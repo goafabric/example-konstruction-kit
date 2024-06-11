dcc77fe15c6b6c3304371ba9ba3ccab8fada51ec
Andreas Mautsch <amautsch>
2024-06-11 08:26:40 +0200

    changes

--- terraform/infra/istio/label.sh
+++ terraform/infra/istio/label.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-kubectl create namespace core; kubectl label namespace core istio.io/dataplane-mode=ambient
-kubectl create namespace example; kubectl label namespace example istio.io/dataplane-mode=ambient
-kubectl create namespace event; kubectl label namespace event istio.io/dataplane-mode=ambient
-kubectl create namespace invoice; kubectl label namespace invoice istio.io/dataplane-mode=ambient
+#kubectl create namespace core; kubectl label namespace core istio.io/dataplane-mode=ambient
+#kubectl create namespace example; kubectl label namespace example istio.io/dataplane-mode=ambient
+#kubectl create namespace event; kubectl label namespace event istio.io/dataplane-mode=ambient
+#kubectl create namespace invoice; kubectl label namespace invoice istio.io/dataplane-mode=ambient
 
-kubectl create namespace monitoring; kubectl label namespace monitoring istio.io/dataplane-mode=ambient
+#kubectl create namespace monitoring; kubectl label namespace monitoring istio.io/dataplane-mode=ambient
 
 kubectl label pods -l app.kubernetes.io/name=apisix istio.io/dataplane-mode= -n ingress-apisix --overwrite
 kubectl create namespace kong; kubectl label namespace kong istio.io/dataplane-mode=ambient --overwrite
