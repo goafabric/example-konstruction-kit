f3407654dd3a571f123692c25635e830cd6a2048
Andreas Mautsch <amautsch>
2024-06-18 13:49:46 +0200

    changes

--- terraform/infra/ingress/kong/archive/gateway-kcrd.tf
+++ terraform/infra/ingress/kong/archive/gateway-kcrd.tf
@@ -1,11 +1,11 @@
 resource "terraform_data" "gateway_crd" {
   provisioner "local-exec" {
     when = create
-    command = "kubectl apply -f https://github.com/kubernetes-sigs/gateway-api/releases/download/v1.0.0/standard-install.yaml && kubectl apply -f ./templates/gateway-class.yaml"
+    command = "kubectl apply -f https://github.com/kubernetes-sigs/gateway-api/releases/download/v1.0.0/standard-install.yaml && kubectl apply -f ./archive/gateway-class.yaml"
   }
 
   provisioner "local-exec" {
     when = destroy
-    command = "kubectl delete -f ./templates/gateway-class.yaml && kubectl delete -f https://github.com/kubernetes-sigs/gateway-api/releases/download/v1.0.0/standard-install.yaml"
+    command = "kubectl delete -f ./archive/gateway-class.yaml && kubectl delete -f https://github.com/kubernetes-sigs/gateway-api/releases/download/v1.0.0/standard-install.yaml"
   }
 }
