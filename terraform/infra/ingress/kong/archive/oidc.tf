a2b540ee31ab652c2d31327a81323ddfc2a18252
Andreas Mautsch <amautsch>
2024-06-18 09:21:15 +0200

    changes

--- terraform/infra/ingress/kong/archive/oidc.tf
+++ terraform/infra/ingress/kong/archive/oidc.tf
@@ -1,15 +1,15 @@
-resource "kubernetes_manifest" "grafana-gateway" {
+resource "kubernetes_manifest" "oidc-plugin" {
   manifest   = yamldecode(<<-EOF
   apiVersion: configuration.konghq.com/v1
   kind: KongPlugin
   metadata:
-    name: {{ include "application.fullname" . }}-oidc-plugin
+    name: oidc-plugin
+    namespace: example
   plugin: oidc
   config:
     client_id: oauth2-proxy
     client_secret: none
     discovery: http://keycloak-application.oidc:8080/oidc/realms/tenant-0/.well-known/openid-configuration
-    logout_path: "/logout"
   EOF
   )
 }
