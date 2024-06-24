5bda689d35a4c7b409dd4de59fab778aaca91d7b
Andreas Mautsch <amautsch>
2024-06-24 08:05:47 +0200

    changes

--- doc/jmeter/loadtest-remote.js
+++ doc/jmeter/loadtest-remote.js
@@ -6,6 +6,29 @@ export const options = {
     duration: '1s', // Duration of the test
 };
 
+export default function () {
+    const headers = {
+        'Authorization': `Bearer ${getAccessToken()}`,
+    };
+
+    const requestOptions = {
+        headers: headers,
+        redirects: 0, // Disable automatic following of redirects
+    };
+
+    checkResponse(http.get("https://v2202402203466256255.megasrv.de/core/patients/findByGivenName?givenName=S", requestOptions));
+    checkResponse(http.get("https://v2202402203466256255.megasrv.de/catalog/insurances/findByDisplay?display=a", requestOptions));
+}
+
+function checkResponse(response) {
+    check(response, {
+        'status is 200': (r) => r.status === 200,
+    });
+    if (response.status !== 200) {
+        console.error(`Unexpected status for second request: ${response.status}`);
+    }
+}
+
 function getAccessToken() {
     const url = 'https://v2202402203466256255.megasrv.de/oidc/realms/tenant-0/protocol/openid-connect/token';
     const headers = {
@@ -26,35 +49,5 @@ function getAccessToken() {
         return null;
     }
 
-    const token = JSON.parse(response.body).access_token;
-    return token;
-}
-
-export default function () {
-    const headers = {
-        'Authorization': `Bearer ${getAccessToken()}`,
-    };
-
-    const requestOptions = {
-        headers: headers,
-        redirects: 0, // Disable automatic following of redirects
-    };
-
-    const response1 = http.get("https://v2202402203466256255.megasrv.de/core/patients/findByGivenName?givenName=S", requestOptions);
-    check(response1, {
-        'status is 200': (r) => r.status === 200,
-    });
-
-    if (response1.status !== 200) {
-        console.error(`Unexpected status for first request: ${response1.status}`);
-    }
-
-    const response2 = http.get("https://v2202402203466256255.megasrv.de/catalog/insurances/findByDisplay?display=a", requestOptions);
-    check(response2, {
-        'status is 200': (r) => r.status === 200,
-    });
-
-    if (response2.status !== 200) {
-        console.error(`Unexpected status for second request: ${response2.status}`);
-    }
+    return JSON.parse(response.body).access_token;
 }
