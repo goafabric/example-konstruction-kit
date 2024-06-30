de8f87686d3a2100cd1f2b5e74450f7de2a555df
Andreas Mautsch <amautsch>
2024-06-30 18:25:54 +0200

    changes

--- doc/release-notes.md
+++ doc/release-notes.md
@@ -1,5 +1,6 @@
 # 1.3.4
 - back to stable invoice release
+- Tech Radar added
 
 # 1.3.3
 - kong oidc integration
--- terraform/infra/ingress/cert-manager/variables.tf
+++ terraform/infra/ingress/cert-manager/variables.tf
@@ -4,5 +4,5 @@ variable "hostname" {
 
 locals {
   production_mode = !strcontains(var.hostname, ".local")
-  cert_manager_issuer = "./cert-manager-issuer/selfsigned" #local.production_mode == true ? "./cert-manager-issuer/letsencrypt" : "./cert-manager-issuer/selfsigned"
+  cert_manager_issuer = local.production_mode == true ? "./cert-manager-issuer/letsencrypt" : "./cert-manager-issuer/selfsigned"
 }
