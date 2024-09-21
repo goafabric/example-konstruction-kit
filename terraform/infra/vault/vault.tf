17a2631630f03c0fef75e78380c8491a92cd8f8a
Andreas Mautsch <amautsch>
2024-09-21 16:54:11 +0200

    apisix

--- terraform/infra/vault/vault.tf
+++ terraform/infra/vault/vault.tf
@@ -16,5 +16,25 @@ resource "helm_release" "hashicorp_vault" {
 
 #helm repo add hashicorp https://helm.releases.hashicorp.com
 #helm install vault hashicorp/vault --namespace vault --create-namespace --version 0.28.1
-
 #helm uninstall vault --namespace vault
+
+# kubectl -n vault port-forward svc/vault 8200:8200
+
+# kubectl exec --stdin=true --tty=true vault-0 -n vault -- /bin/sh
+# vault operator init
+# vault operator unseal 5 times
+
+# vault login
+# vault auth enable kubernetes
+
+## injection config
+vault write auth/kubernetes/config \
+token_reviewer_jwt="$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)" \
+kubernetes_host="https://${KUBERNETES_PORT_443_TCP_ADDR}:443" \
+kubernetes_ca_cert=@/var/run/secrets/kubernetes.io/serviceaccount/ca.crt
+
+## https://github.com/marcel-dempers/docker-development-youtube-series/tree/master/hashicorp/vault-2022/example-apps/basic-secret
+
+kubectl -n example apply -f ./example-apps/deployment.yaml
+kubectl -n example get pods
+"cat /vault/secrets/helloworld"
