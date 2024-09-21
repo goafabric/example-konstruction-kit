b04f509d9a8e248842521529c2f146b0d561e56a
Andreas Mautsch <amautsch>
2024-09-21 17:45:30 +0200

    apisix

--- terraform/infra/vault/vault.tf
+++ terraform/infra/vault/vault.tf
@@ -37,6 +37,7 @@ kubernetes_ca_cert=@/var/run/secrets/kubernetes.io/serviceaccount/ca.crt
 
 kubectl -n example apply -f ./example-apps/deployment.yaml
 kubectl -n example get pods
-"cat /vault/secrets/helloworld"
+
+cat /vault/secrets/helloworld
 
 kubectl -n example delete -f ./example-apps/deployment.yaml
