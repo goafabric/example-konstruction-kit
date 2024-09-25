61d161f091474eff5712f114b40830fde35f5afd
Andreas Mautsch <amautsch>
2024-09-25 16:11:56 +0200

    changes

--- terraform/infra/vault/vault-configure.tf
+++ terraform/infra/vault/vault-configure.tf
@@ -10,12 +10,14 @@ resource "kubernetes_service_account" "vault_read_account" {
   }
 }
 
-# resource "vault_kv_secret" "my_secret" {
-#   path = "secret/application/callee-service-application"
-#
-#   data_json = jsonencode({
-#     username = "dbuser"
-#     password = "sUp3rS3cUr3P@ssw0rd"
-#   })
-# }
-#
+resource "vault_kv_secret" "my_secret" {
+  path = "secret/data/application/my-service-application"
+
+  data_json = jsonencode({
+    data = {
+      username = "myuser"
+      password = "sUp3rS3cUr3P@ssw0rd"
+    }
+  })
+}
+
