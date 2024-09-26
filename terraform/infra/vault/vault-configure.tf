8787abfa1e47d2bfd64a3780a3d78788eb7cba1d
Andreas Mautsch <amautsch>
2024-09-26 09:01:42 +0200

    bank vault

--- terraform/infra/vault/vault-configure.tf
+++ terraform/infra/vault/vault-configure.tf
@@ -3,30 +3,7 @@ provider "vault" {
   token   = "root"
 }
 
-## vault kubernetes auth bullshit
-
-resource "vault_auth_backend" "kubernetes" {
-  depends_on = [helm_release.vault]
-
-  type = "kubernetes"
-  path = "kubernetes"
-}
-
-resource "terraform_data" "vault_k8s_config" {
-  depends_on = [helm_release.vault]
-  provisioner "local-exec" {
-    command = <<EOT
-kubectl exec -it vault-0 -n vault -- /bin/sh -c '
-vault write auth/kubernetes/config \
-  token_reviewer_jwt="$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)" \
-  kubernetes_host="https://$${KUBERNETES_PORT_443_TCP_ADDR}:443" \
-  kubernetes_ca_cert=@/var/run/secrets/kubernetes.io/serviceaccount/ca.crt
-'
-EOT
-  }
-}
-
-## service account per namespace
+## kubernetes service account per namespace, might be better suited inside namespace creation
 
 variable "namespaces" {
   type    = set(string)
@@ -42,10 +19,33 @@ resource "kubernetes_service_account" "vault_read_account" {
   }
 }
 
+## vault kubernetes auth bullshit
 
-# create read role + policy
-resource "vault_kubernetes_auth_backend_role" "vault_read_role" {
+resource "vault_auth_backend" "kubernetes" {
   depends_on = [helm_release.vault]
+
+  type = "kubernetes"
+  path = "kubernetes"
+}
+
+resource "terraform_data" "vault_k8s_config" {
+  depends_on = [vault_auth_backend.kubernetes]
+  provisioner "local-exec" {
+    command = <<EOT
+kubectl exec -it vault-0 -n vault -- /bin/sh -c '
+vault write auth/kubernetes/config \
+  token_reviewer_jwt="$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)" \
+  kubernetes_host="https://$${KUBERNETES_PORT_443_TCP_ADDR}:443" \
+  kubernetes_ca_cert=@/var/run/secrets/kubernetes.io/serviceaccount/ca.crt
+'
+EOT
+  }
+}
+
+# vault read role + policy
+
+resource "vault_kubernetes_auth_backend_role" "vault_read_role" {
+  depends_on = [terraform_data.vault_k8s_config]
 
   role_name     = "vault-read-role"
   backend       = "kubernetes"
@@ -56,7 +56,7 @@ resource "vault_kubernetes_auth_backend_role" "vault_read_role" {
 }
 
 resource "vault_policy" "vault_read_policy" {
-  depends_on = [helm_release.vault]
+  depends_on = [terraform_data.vault_k8s_config]
   name = "vault-read-policy"
 
   policy = <<EOT
@@ -66,7 +66,24 @@ path "secret/data/database/*" {
 EOT
 }
 
-resource "vault_kv_secret" "my_secret" {
+## secrets
+
+resource "vault_kv_secret" "example-service-postgres" {
+  depends_on = [helm_release.vault]
+  path = "secret/data/database/example-service-postgres"
+
+  data_json = jsonencode({
+    data = {
+      POSTGRES_USER = "example-service"
+      POSTGRES_PASSWORD = "sUp3rS3cUr3Passw0rd"
+      "spring.datasource.username" = "example-service"
+      "spring.datasource.password" = "sUp3rS3cUr3Passw0rd"
+    }
+  })
+}
+
+
+resource "vault_kv_secret" "person-service-postgres" {
   depends_on = [helm_release.vault]
   path = "secret/data/database/person-service-postgres"
 
