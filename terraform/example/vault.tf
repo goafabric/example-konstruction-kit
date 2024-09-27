1091720418cf3605a3a55b6cd31734873963e017
Andreas Mautsch <amautsch>
2024-09-27 08:13:36 +0200

    changes

--- terraform/example/vault.tf
+++ terraform/example/vault.tf
@@ -2,7 +2,7 @@ resource "terraform_data" "vault-create-person-service-postgres" {
   provisioner "local-exec" {
     when = create
     command = <<EOT
-kubectl exec -it vault-0 -n vault -- sh -c 'U=person-service;P=$(cat /proc/sys/kernel/random/uuid | tr -d '-' | sha256sum | base64 | head -c 32);
+kubectl exec vault-0 -n vault -- sh -c 'U=person-service;P=$(cat /proc/sys/kernel/random/uuid | tr -d '-' | sha256sum | base64 | head -c 32);
   vault kv put databases/person-service-postgres POSTGRES_USER=$U POSTGRES_PASSWORD=$P spring.datasource.username=$U spring.datasource.password=$P;'
 EOT
   }
@@ -11,6 +11,6 @@ EOT
 resource "terraform_data" "vault-destroy-person-service-postgres" {
   provisioner "local-exec" {
     when = destroy
-    command = "kubectl exec -it vault-0 -n vault -- sh -c 'vault kv delete databases/person-service-postgres'"
+    command = "kubectl exec vault-0 -n vault -- sh -c 'vault kv delete databases/person-service-postgres'"
   }
 }
