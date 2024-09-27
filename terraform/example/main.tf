494e12884a7ccc611dcd236bae85a74da0f8873f
Andreas Mautsch <amautsch>
2024-09-27 08:03:16 +0200

    changes

--- terraform/example/main.tf
+++ terraform/example/main.tf
@@ -17,3 +17,12 @@ resource "random_password" "oidc_session_secret" {
   length           = 32
   special          = false
 }
+
+resource "terraform_data" "vault-person-service-postgres" {
+  provisioner "local-exec" {
+    command = <<EOT
+kubectl exec -it vault-0 -n vault -- sh -c 'U=person-service;P=$(cat /proc/sys/kernel/random/uuid | tr -d '-' | sha256sum | base64 | head -c 32);
+  vault kv put databases/person-service-postgres POSTGRES_USER=$U POSTGRES_PASSWORD=$P spring.datasource.username=$U spring.datasource.password=$P;'
+EOT
+  }
+}
