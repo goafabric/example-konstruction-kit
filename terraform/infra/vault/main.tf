fc2abbad45db685602097bd51416f689715d4bcb
Andreas Mautsch <amautsch>
2024-09-28 14:27:08 +0200

    changes

--- terraform/infra/vault/main.tf
+++ terraform/infra/vault/main.tf
@@ -7,8 +7,3 @@ provider "helm" {
 provider "kubernetes" {
   config_path = "~/.kube/config"
 }
-
-resource "random_password" "database_password" {
-  length           = 32
-  special          = false
-}
