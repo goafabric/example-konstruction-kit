2f9be3250496c7ee83e394fb6b32eb6c2455fc39
Andreas Mautsch <amautsch>
2024-09-30 10:51:03 +0200

    changes

--- terraform/infra/vault/configure/vault-configure.tf
+++ terraform/infra/vault/configure/vault-configure.tf
@@ -32,7 +32,7 @@ resource "terraform_data" "vault_k8s_config" {
   provisioner "local-exec" {
     command = <<EOT
 kubectl exec vault-0 -n vault -- /bin/sh -c '
-vault auth enable kubernetes && \
+export VAULT_TOKEN= && vault auth enable kubernetes && \
 vault write auth/kubernetes/config \
   token_reviewer_jwt="$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)" \
   kubernetes_host="https://$${KUBERNETES_PORT_443_TCP_ADDR}:443" \
