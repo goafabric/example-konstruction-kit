c670e272566fb50398bd7a36f440aa67451ab134
Andreas Mautsch <amautsch>
2025-01-19 08:27:48 +0100

    changes

--- doc/scripts/export-s3.sh
+++ doc/scripts/export-s3.sh
@@ -10,9 +10,11 @@ kubectl delete pod s3-export -n core &> /dev/null
 
 kubectl run -i --rm --tty --namespace core s3-export --image=busybox --restart=Never -- /bin/sh -c "
   wget -qO- https://dl.min.io/client/mc/release/linux-arm64/mc > /usr/bin/mc && chmod +x /usr/bin/mc
-
   mc alias set myminio $S3_URL minioadmin $PASSWORD &&
+
   mc cp --recursive myminio/core-$TENANT_ID /tmp/ &&
+
+  
   echo 'export done .. please manually execute the following and terminate this pod with ctrl-c' &&
   echo 'kubectl cp core/s3-export:/tmp $LOCAL_DOWNLOAD_FOLDER/s3' &&
   sleep 6000000 #tail -f /dev/null
