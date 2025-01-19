62074c3e086460c71f164ab7628c88bf27cdb32d
Andreas Mautsch <amautsch>
2025-01-19 08:12:05 +0100

    changes

--- doc/scripts/export-s3.sh
+++ doc/scripts/export-s3.sh
@@ -5,8 +5,10 @@ PASSWORD=minioadmin
 
 kubectl delete pod s3-export -n core &> /dev/null
 
-kubectl run s3-export -n core --rm -it --restart=Never --image=minio/mc --command -- /bin/sh -c "
+kubectl run s3-export -n core -it --restart=Never --image=minio/mc --command -- /bin/sh -c "
   mc alias set myminio http://s3-minio.core:9000 minioadmin $PASSWORD &&
   mc cp --recursive myminio/core-$TENANT_ID /tmp/ &&
-  echo 'Commands executed successfully!'
+  echo 'export done .. please manually execute the following and terminate this pod with ctrl-c' &&
+  echo 'kubectl cp core/s3-export:/tmp ~/Downloads/s3' &&
+  sleep 60000 #tail -f /dev/null
 "
