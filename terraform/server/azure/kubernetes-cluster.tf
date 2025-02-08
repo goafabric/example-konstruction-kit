e03f9a29c8b96798847eb8f58f8da0ab40e19c43
Andreas Mautsch <amautsch>
2025-02-08 08:33:36 +0100

    azure

--- terraform/server/azure/kubernetes-cluster.tf
+++ terraform/server/azure/kubernetes-cluster.tf
@@ -4,15 +4,16 @@ resource "random_pet" "azurerm_kubernetes_cluster_dns_prefix" {
 }
 
 resource "azurerm_resource_group" "rg" {
-  location = local.resource_group_location
-  name     = local.resource_group_name
+  location = var.resource_group_location
+  name     = var.resource_group_name
 }
 
 resource "azurerm_kubernetes_cluster" "k8s" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  name                = local.cluster_name
+  name                = azurerm_resource_group.rg.name
   dns_prefix          = random_pet.azurerm_kubernetes_cluster_dns_prefix.id
+  kubernetes_version  = "1.30"
 
   identity {
     type = "SystemAssigned"
@@ -24,16 +25,19 @@ resource "azurerm_kubernetes_cluster" "k8s" {
     node_count = var.default_node_count
   }
 
-  linux_profile {
-    admin_username = var.username
-
-    ssh_key {
-      key_data = azapi_resource_action.ssh_public_key_gen.output.publicKey
-    }
-  }
+  # linux_profile {
+  #   admin_username = var.username
+  #
+  #   ssh_key {
+  #     key_data = azapi_resource_action.ssh_public_key_gen.output.publicKey
+  #   }
+  # }
 
   network_profile {
-    network_plugin    = "kubenet"
+    network_plugin      = "azure"
+    network_policy      = "calico"
+    network_plugin_mode = "overlay"
+
     load_balancer_sku = "standard"
   }
 }
--- terraform/server/azure/variables.tf
+++ terraform/server/azure/variables.tf
@@ -1,31 +1,26 @@
+#should be set from environment
+variable "subscription_id" {
+  type        = string
+}
+
 variable "default_node_count" {
   type        = number
-  description = "The initial quantity of nodes for the node pool."
   default     = 1
 }
 
 variable "default_vm_size" {
   type        = string
-  description = "VM SIZE"
   default     = "Standard_D4ds_v5"
 }
 
-variable "username" {
+variable "resource_group_name" {
   type        = string
-  description = "The admin username for the new cluster."
-  default     = "azureadmin"
+  default     = "eden-eve"
 }
 
-#should be set from environment
-variable "subscription_id" {
+variable "resource_group_location" {
   type        = string
-  description = "The azure Subscription ID"
-}
-
-locals {
-  resource_group_name = "eden-eve"
-  cluster_name = local.resource_group_name
-  resource_group_location = "Germany West Central"
+  default     = "Germany West Central"
 }
 
 # terraform output -raw kube_config > ~/.kube/config
