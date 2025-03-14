c2450bc7986efcc7d20d9ddfcdd79184e41cbc57
Andreas Mautsch <amautsch>
2025-03-14 16:44:47 +0100

    dragonfly added

--- terraform/infra/istio/prometheus.tf
+++ terraform/infra/istio/prometheus.tf
@@ -2,7 +2,7 @@ resource "helm_release" "prometheus" {
   repository = "https://prometheus-community.github.io/helm-charts"
   name       = "prometheus"
   chart      = "prometheus"
-  version    = "25.27.0"
+  version    = "26.1.0"
   namespace  = "istio-system"
   create_namespace = false
 
