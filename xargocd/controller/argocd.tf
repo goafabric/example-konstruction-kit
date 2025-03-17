27c59ec8648b340dadf9cb1becd26c534860bc7b
Andreas Mautsch <amautsch>
2025-03-17 13:00:35 +0100

    changes

--- xargocd/controller/argocd.tf
+++ xargocd/controller/argocd.tf
@@ -1,5 +1,5 @@
 resource "helm_release" "argocd" {
-  name       = "argo-cd"
+  name       = "argocd"
   repository = "https://argoproj.github.io/argo-helm"
   chart      = "argo-cd"
   namespace  = "argocd"
@@ -15,4 +15,67 @@ resource "helm_release" "argocd" {
 #    name  = "configs.cm.timeout.reconciliation"
 #    value = "20s"
 #  }
+
+
+  set {
+    name  = "configs.params.server\\.insecure"
+    value = true
+  }
+
+  set {
+    name  = "global.domain"
+    value = var.hostname
+  }
+
+  set {
+    name  = "configs.params.server\\.basehref"
+    value = "/argocd"
+  }
+
+  set {
+    name  = "notifications.cm.create"
+    value = "false"
+  }
+
+  set {
+    name  = "global.addPrometheusAnnotations"
+    value = "true"
+  }
+
+  set {
+    name  = "controller.metrics.enabled"
+    value = "true"
+  }
+}
+
+
+resource "kubernetes_manifest" "argocd-ingress" {
+  manifest   = yamldecode(<<-EOF
+  kind: Ingress
+  apiVersion: networking.k8s.io/v1
+  metadata:
+    name: argocd-ingress
+    namespace: argocd
+    annotations:
+      cert-manager.io/cluster-issuer: my-cluster-issuer
+      konghq.com/strip-path: 'true'
+  spec:
+    ingressClassName: kong
+    tls:
+      - hosts:
+          - ${var.hostname}
+        secretName: root-certificate
+    rules:
+      - host: ${var.hostname}
+        http:
+          paths:
+            - path: /argocd
+              pathType: ImplementationSpecific
+              backend:
+                service:
+                  name: argocd-server
+                  port:
+                    number: 80
+  EOF
+  )
 }
