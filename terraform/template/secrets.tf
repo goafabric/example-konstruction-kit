a8481d81c0ca5bc85b795be5c31af7072cebd948
Andreas Mautsch <amautsch>
2025-04-07 11:23:06 +0200

    changes

--- terraform/template/secrets.tf
+++ terraform/template/secrets.tf
@@ -7,3 +7,23 @@ resource "random_password" "oidc_session_secret" {
   length           = 32
   special          = false
 }
+
+
+resource "kubernetes_secret" "image_registry_secret" {
+  metadata {
+    name      = "image-registry-secret"
+    namespace = "core"
+  }
+
+  type = "kubernetes.io/dockerconfigjson"
+
+  data = {
+    ".dockerconfigjson" = jsonencode({
+      auths = {
+        (var.image_registry) = {
+          "auth" = base64encode("${var.image_registry_user}:${var.image_registry_password}")
+        }
+      }
+    })
+  }
+}
--- terraform/template/variables.tf
+++ terraform/template/variables.tf
@@ -10,6 +10,14 @@ variable "image_registry" {
   default = "goafabric"
 }
 
+variable "image_registry_user" {
+  default = "goafabric"
+}
+
+variable "image_registry_password" {
+  default = "goafabric"
+}
+
 data "external" "server_arch_data" {
   program = ["sh", "-c", "if kubectl version --output=json | grep -q 'linux/arm64'; then echo '{\"server_arch\":\"-arm64v8\"}'; else echo '{\"server_arch\":\"\"}'; fi"]
 }
