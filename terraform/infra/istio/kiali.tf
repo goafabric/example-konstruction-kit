a8a4042c9285f5fdfbc80ccc8315366badc66aae
Andreas Mautsch <amautsch>
2025-04-25 09:23:24 +0200

    upgrades

--- terraform/infra/istio/kiali.tf
+++ terraform/infra/istio/kiali.tf
@@ -4,7 +4,7 @@ resource "helm_release" "kiali" {
   chart      = "kiali-server"
   namespace  = "istio-system"
   create_namespace = false
-  version    = "2.5.0"
+  version    = "2.8.0"
   wait       = true
 
   set {
