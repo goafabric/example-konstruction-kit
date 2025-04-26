7b3216222f063c7774ddd74c8ce3d6083676e5a0
Andreas Mautsch <amautsch>
2025-04-26 16:55:18 +0200

    upgrades

--- terraform/event/kafka.tf
+++ terraform/event/kafka.tf
@@ -44,7 +44,7 @@ resource "helm_release" "kafka" {
   }
   set {
     name  = "metrics.jmx.enabled"
-    value = true
+    value = false
   }
   set {
     name  = "commonLabels.app"
@@ -56,6 +56,6 @@ resource "helm_release" "kafka" {
 resource "terraform_data" "remove_kafka_pvc" {
   provisioner "local-exec" {
     when = destroy
-    command = "kubectl delete pvc -l app.kubernetes.io/instance=kafka -n event"
+    command = "kubectl delete pvc -l app.kubernetes.io/instance=kafka -n data"
   }
 }
