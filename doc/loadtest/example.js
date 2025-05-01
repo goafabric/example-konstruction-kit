e564d3f93e14390e084540e58b39e8b9a3d5c3f7
Andreas Mautsch <amautsch>
2025-05-01 15:55:13 +0200

    upgrades

--- doc/loadtest/example.js
+++ doc/loadtest/example.js
@@ -5,13 +5,13 @@ import http from "k6/http";
 import { check, sleep } from 'k6';
 
 export const options = {
-    vus: 10, // Number of virtual users
+    vus: 1, // Number of virtual users
     duration: '10s', // Duration of the test
 };
 
-const baseUrl = 'https://v2202402203466256255.megasrv.de'; //'https://kind.local'
+const baseUrl = 'https://kind.local'
 
 export default function () {
     http.get(`${baseUrl}/person/persons/findByFirstName?firstName=Homer`);
-    http.get(`${baseUrl}/person/persons/name?name=Heisenberg`);
+    http.get(`${baseUrl}/person/persons/sayMyName?name=Heisenberg`);
 }
