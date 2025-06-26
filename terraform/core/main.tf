6b1d1fcc04ad530911ea31322c33d37855b5101c
Andreas Mautsch <amautsch>
2025-06-26 09:55:31 +0200

    chang

--- terraform/core/main.tf
+++ terraform/core/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    helm = {
+      source  = "hashicorp/helm"
+      version = "2.12.0"
+    }
+  }
+}
+
 provider "helm" {
   kubernetes {
     config_path = "~/.kube/config"
