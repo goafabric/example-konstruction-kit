2ca248ecd3be5245209405a174645d656d3dcade
Andreas Mautsch <amautsch>
2025-07-07 16:06:51 +0200

    secrets

--- xargocd/invoice/secrets.tf
+++ xargocd/invoice/secrets.tf
@@ -7,3 +7,18 @@ resource "random_password" "oidc_session_secret" {
   length           = 32
   special          = false
 }
+
+resource "kubernetes_secret" "redis_secret" {
+  metadata {
+    name      = "redis-secret"
+    namespace = "invoice"
+  }
+
+  data = {
+    "spring.data.redis.password" = random_password.cache_password.result
+
+    #"spring.data.redis.sentinel.password" = random_password.redis_password.result
+  }
+
+  type = "Opaque"
+}
