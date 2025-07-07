50ed626b912cec11c82378cc7cc7f5b774ef40dd
Andreas Mautsch <amautsch>
2025-07-07 10:21:43 +0200

    secrets

--- terraform/invoice/invoice-process.tf
+++ terraform/invoice/invoice-process.tf
@@ -29,11 +29,6 @@ resource "helm_release" "invoice-process-application" {
   }
 
   set_sensitive {
-    name  = "s3.password"
-    value = data.kubernetes_secret.s3_secret.data["password"]
-  }
-  
-  set_sensitive {
     name  = "redis.password"
     value = random_password.cache_password.result
   }
