d1fbefafdc84d8cc05d60b45e95b9a1b606ca9e8
Andreas Mautsch <amautsch>
2025-11-19 07:59:03 +0100

    upgrades

--- terraform/infra/oidc/main.tf
+++ terraform/infra/oidc/main.tf
@@ -17,6 +17,12 @@ provider "kubernetes" {
   config_path = "~/.kube/config"
 }
 
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
+
 provider "keycloak" {
   client_id     = "admin-cli"
   username      = "admin"
