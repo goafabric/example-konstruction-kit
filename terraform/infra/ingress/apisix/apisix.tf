2bcc6d0388b95b3b067a88b3a146aa77e0ccd186
Andreas Mautsch <amautsch>
2025-12-14 11:39:54 +0100

    boot 4.0

--- terraform/infra/ingress/apisix/apisix.tf
+++ terraform/infra/ingress/apisix/apisix.tf
@@ -165,3 +165,4 @@ resource "terraform_data" "re-init_ingress_controller" {
 }
 
 #kubectl -n ingress-apisix patch deployment apisix-ingress-controller --type='json' -p='[{"op":"replace","path":"/spec/template/spec/initContainers/0/image","value":"curlimages/curl:8.5.0"},{"op":"replace","path":"/spec/template/spec/initContainers/0/command","value":["sh","-c","until curl -s -o /dev/null http://apisix-admin.ingress-apisix.svc.cluster.local:9180; do echo waiting for apisix-admin; sleep 2; done"]}]'
+#kubectl -n ingress-apisix patch deployment apisix-ingress-controller --type='json' -p='[{"op":"replace","path":"/spec/template/spec/initContainers/0/image","value":"busybox:1.28"},{"op":"replace","path":"/spec/template/spec/initContainers/0/command","value":["sh","-c","until nc -z apisix-admin.ingress-apisix.svc.cluster.local 9180; do echo waiting for apisix-admin; sleep 2; done"]}]'
