d35f8cf4cdba2b02230fad56e769f375b491d076
Andreas Mautsch <amautsch>
2026-01-16 12:54:15 +0100

    chang

--- terraform/infra/ingress/apisix/apisix.tf
+++ terraform/infra/ingress/apisix/apisix.tf
@@ -2,7 +2,7 @@ resource "helm_release" "apisix" {
   name       = "apisix"
   repository = "https://apache.github.io/apisix-helm-chart"
   chart      = "apisix"
-  version    = "2.11.0" # "2.12.5"
+  version    = "2.12.5" #"2.11.0"
   namespace  = "ingress-apisix"
   timeout    = "90"
   create_namespace = false
