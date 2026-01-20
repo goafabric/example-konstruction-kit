71fbe7683d5bd83740af2f9691038e54c1d298bb
Andreas Mautsch <amautsch>
2026-01-20 14:46:32 +0100

    chang

--- terraform/infra/istio/kiali.tf
+++ terraform/infra/istio/kiali.tf
@@ -102,33 +102,3 @@ resource "kubernetes_manifest" "kiali-route" {
   EOF
   )
 }
-
-# resource "kubernetes_manifest" "kiali-ingress" {
-#   manifest   = yamldecode(<<-EOF
-#   kind: Ingress
-#   apiVersion: networking.k8s.io/v1
-#   metadata:
-#     name: kiali-ingress
-#     namespace: istio-system
-#     annotations:
-#       cert-manager.io/cluster-issuer: my-cluster-issuer
-#   spec:
-#     ingressClassName: kong
-#     tls:
-#       - hosts:
-#           - ${var.hostname}
-#         secretName: root-certificate
-#     rules:
-#       - host: ${var.hostname}
-#         http:
-#           paths:
-#             - path: /kiali
-#               pathType: ImplementationSpecific
-#               backend:
-#                 service:
-#                   name: kiali
-#                   port:
-#                     number: 20001
-#   EOF
-#   )
-# }
