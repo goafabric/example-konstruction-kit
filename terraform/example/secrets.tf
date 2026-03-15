6fe4621c7f14c7b395e7b5e6b74d6d8724a9d3c5
Andreas Mautsch <amautsch>
2026-03-15 09:40:49 +0100

    quarkus

--- terraform/example/secrets.tf
+++ terraform/example/secrets.tf
@@ -23,8 +23,8 @@ resource "kubernetes_secret" "postgresql_secret" {
     "spring.datasource.username" = "main"
     "spring.datasource.password" = random_password.postgresql_password.result
 
-    "quarkus.datasource.username" = "main"
-    "quarkus.datasource.password" = random_password.postgresql_password.result
+    #"quarkus.datasource.username" = "main"
+    #"quarkus.datasource.password" = random_password.postgresql_password.result
   }
 
   type = "Opaque"
