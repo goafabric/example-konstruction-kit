9ceb20291a958442e4b43ec2ab76e40ff3535387
Andreas Mautsch <amautsch>
2026-03-15 10:55:50 +0100

    quarkus

---
+++ terraform/data/azurite.tf
@@ -0,0 +1,53 @@
+resource "helm_release" "azurite" {
+  name       = "azurite"
+  repository = "oci://ghcr.io/emberstack/helm-charts"
+  chart      = "azurite"
+  namespace  = "data"
+  version    = "1.0.19"
+  timeout = 60
+
+  set {
+    name  = "config.extraEnvVars[0].name"
+    value = "TZ"
+  }
+  set {
+    name  = "config.extraEnvVars[0].value"
+    value = "Europe/Berlin"
+  }
+  
+  set {
+    name  = "readinessProbe.initialDelaySeconds"
+    value = "2"
+  }
+
+  set {
+    name  = "configuration.blobEnabled"
+    value = true
+  }
+
+  set {
+    name  = "configuration.queueEnabled"
+    value = false
+  }
+
+  set {
+    name  = "configuration.tableEnabled"
+    value = false
+  }
+
+
+  set {
+    name = "podLabels.app"
+    value = "azurite"
+  }
+
+}
+
+# manually remove the pvc to avoid password problems
+resource "terraform_data" "remove_azurite_pvc" {
+
+  provisioner "local-exec" {
+    when = destroy
+    command = "kubectl delete pvc azurite -n data"
+  }
+}
